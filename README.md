8f2d7b5e8bc0b6b51080c58aa88433fb0dd8b05c
Otto Kivikärki <otto.kivikarki@vincit.fi>
2018-08-12 20:46:50 +0300

    Update deps and readme. Still works.

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Learning about microservices with nodejs + express and
 handling intercommunication through RabbitMQ. Using an
 api-gateway for external communication.
+
+---
+
+Users service waits for messages from the api-gateway.
+
+POST payload to /user endpoint, users service receives it and
+acknowledges it by logging a message to the server console.
--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -1,32 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const debug = require('debug')('api-gateway:routes');
+const debug = require("debug")("api-gateway:routes");
 
-const amqplib = require('amqplib');
+const amqplib = require("amqplib");
 
-const QUEUE_NAME = 'users';
-const MOCK_DATA = JSON.stringify([{ id: 1, username: 'ottoki' }]);
+const QUEUE_NAME = "users";
 
-router.get('/', function (req, res, next) {
-  res.json({ message: 'ok' });
+router.get("/", function(req, res, next) {
+  res.json({ message: "ok" });
 });
 
-router.get('/users', function (req, res, next) {
-  amqplib.connect('amqp://rabbitmq:rabbitmq@localhost:5672/rabbit_node_1')
+router.post("/user", function(req, res, next) {
+  amqplib
+    .connect("amqp://rabbitmq:rabbitmq@localhost:5672/rabbit_node_1")
     .then(conn => {
-      return conn.createChannel().then(ch => {
-        const ok = ch.assertQueue(QUEUE_NAME, { durable: false });
+      return conn
+        .createChannel()
+        .then(ch => {
+          const ok = ch.assertQueue(QUEUE_NAME, { durable: false });
 
-        // Queue exist, send a message to it.
-        return ok.then(res => {
-          ch.sendToQueue(QUEUE_NAME, Buffer.from(MOCK_DATA));
-          debug('Sent message to the queue', QUEUE_NAME);
-          return ch.close();
-        });
-      }).finally(() => conn.close());
-    }).catch(console.warn);
+          // Queue exist, send a message to it.
+          return ok.then(res => {
+            ch.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(req.body)));
+            debug("Sent message to the queue", QUEUE_NAME);
+            return ch.close();
+          });
+        })
+        .finally(() => conn.close());
+    })
+    .catch(console.warn);
 
-  res.json({ message: 'ok' });
+  res.json({ message: "ok" });
 });
 
 module.exports = router;
